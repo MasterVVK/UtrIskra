46364db6d2ec2988776274b6a9cb8f42c9309ce5
MasterVVK <vvk@2140101.ru>
2025-01-12 01:24:24 +0500

    up

--- bot/kandinsky_runner.py
+++ bot/kandinsky_runner.py
@@ -1,18 +1,19 @@
 import asyncio
-import os
 import datetime
 from aiogram import Bot
 from aiogram.types import FSInputFile
 from config import TELEGRAM_TOKEN, TARGET_CHAT_ID
 from services.gemini_service import GeminiService
 from services.kandinsky_service import KandinskyService
-import logging
 from utils.database import initialize_database, save_to_database
-from utils.image_utils import save_image_from_base64, add_date_to_image, create_image_path
+from utils.image_utils import add_date_to_image, save_image_from_base64, create_image_path
+from utils.prompt_utils import generate_dynamic_prompt
+import logging
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+PROMPTS_FILE = "prompts/kandinsky_runner.txt"  # Путь к файлу с промптами
 
 async def send_kandinsky_story():
     """Генерация и отправка изображения через Kandinsky API."""
@@ -21,53 +22,42 @@ async def send_kandinsky_story():
     kandinsky_service = KandinskyService()
 
     try:
-        logger.info("Получаем ID модели...")
-        model_id = kandinsky_service.get_model_id()
-        logger.info(f"Проверяем доступность сервиса Kandinsky для model_id={model_id}...")
-        kandinsky_service.check_availability_with_timeout(model_id)
+        # Читаем промпты из файла
+        system_prompt, user_prompt = generate_dynamic_prompt(PROMPTS_FILE)
 
-        current_date = datetime.datetime.now().strftime("%d %B %Y")
-        user_prompt = (
-            f"Today is {current_date}.\n"
-            "Сгенерируй 10 различных вдохновляющих тем для создания иллюстраций, со своей техникой рисования."
-            "Для каждой темы укажи конкретную точку зрения, старайся не повторяться. "
-            "Краткое описание (3–6 предложения и действия) "
-            "Расширенные стилистические ориентиры (жанр, эстетика, детали оформления, цветовая палитра, техникой рисования и т.п.) "
-            "Если в теме вдруг встречаются флаги, они должны быть полностью выдуманными (не связанными с реальными странами). "
-            "Включить хотя бы один интересный элемент, чтобы сделать изображение более интригующим. "
-
-            "Используй номер дня месяца (например, 1…31) и рассчитай индекс как (день % 10)."
-            "Если результат равен 0, выбирай 10-ю тему; "
-            "Если результат не равен 0, выбирай тему с номером, равным результату."
-            "Выведи только выбранную тему. Не нужно выводить список тем и результат расчета. "
-            "Выводи для выбранной темы только текст. "
-            "Ответ должен содержать только текст из 950 символов"
-        )
-        logger.info(f"Генерация текста через Gemini: {user_prompt}")
-
-        system_prompt = "Ответ должен содержать только текст из 950 символов"
+        logger.info(f"Генерация текста через Gemini Pro: {user_prompt}")
         generated_prompt = gemini_service.generate_prompt(
             system_prompt=system_prompt,
             user_prompt=user_prompt,
             temperature=1.0
         )
 
+        # Проверяем длину промпта
         if len(generated_prompt) > 1000:
             logger.warning("Промпт превышает допустимый лимит в 1000 символов. Обрезаем текст.")
             generated_prompt = generated_prompt[:1000]
 
         logger.info(f"Сгенерированный промпт: {generated_prompt}")
+
+        # Работа с Kandinsky API
+        logger.info("Получаем ID модели Kandinsky...")
+        model_id = kandinsky_service.get_model_id()
+        kandinsky_service.check_availability_with_timeout(model_id)
+
+        logger.info("Генерация изображения через Kandinsky...")
         uuid = kandinsky_service.generate_image(generated_prompt, model_id)
 
         logger.info("Ожидание результата генерации...")
         base64_image = kandinsky_service.get_image(uuid, attempts=120, delay=10)
 
-        # Создаем путь для изображения
+        # Сохранение изображения
         image_path = create_image_path(prefix="kandinsky_story")
-
         save_image_from_base64(base64_image, image_path)
-        add_date_to_image(image_path, "K " + datetime.datetime.now().strftime("%d.%m.%Y"))
 
+        current_date_text = "K " + datetime.datetime.now().strftime("%d.%m.%Y")
+        add_date_to_image(image_path, current_date_text)
+
+        # Сохранение в базу данных
         save_to_database(
             date=datetime.datetime.now().strftime("%Y-%m-%d"),
             system_prompt=system_prompt,
@@ -76,12 +66,13 @@ async def send_kandinsky_story():
             image_path=image_path
         )
 
+        # Отправка изображения в Telegram
+        logger.info("Отправка изображения в Telegram...")
         await bot.send_photo(chat_id=TARGET_CHAT_ID, photo=FSInputFile(image_path))
         logger.info("Изображение успешно отправлено!")
     except Exception as e:
         logger.error(f"Ошибка: {e}")
     finally:
-        # Закрываем сессию бота
         await bot.session.close()
 
 if __name__ == "__main__":
